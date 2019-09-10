72f9e3186109bbf1da5b5bf616b7975a784a033b
Han Wang <416810799@qq.com>
2019-09-10 13:08:04 +0200

    soong_config: Add new flag for vendor_init
    Change-Id: I383f1de230ecf7b79d78fe701b140bd1bc6795f2

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -11,3 +11,25 @@ EXPORT_TO_SOONG := \
     TARGET_KERNEL_CONFIG \
     TARGET_KERNEL_SOURCE
 
+# Setup SOONG_CONFIG_* vars to export the vars listed above.
+
+SOONG_CONFIG_NAMESPACES += extraVarsPlugin
+
+SOONG_CONFIG_extraVarsPlugin :=
+
+define addVar
+  SOONG_CONFIG_extraVarsPlugin += $(1)
+  SOONG_CONFIG_extraVarsPlugin_$(1) := $$(subst ",\",$$($1))
+endef
+
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += extraGlobalVars
+SOONG_CONFIG_extraGlobalVars += \
+    target_init_vendor_lib
+
+# Set default values
+TARGET_INIT_VENDOR_LIB ?= vendor_init
+
+# Soong value variables
+SOONG_CONFIG_extraGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
