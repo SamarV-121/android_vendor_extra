9a715b9a35c73e2ef6ef9412a8baaa702cfe7a11
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-01 21:14:29 +0100

    extra: Add wallpaper privapp whitelist
    Change-Id: I044b1d9201ac0b8780fc37a387f401f3dd0ddeac
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- product.mk
+++ product.mk
@@ -1,2 +1,6 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/permissions/privapp_whitelist_com.android.wallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_com.android.wallpaper.xml
