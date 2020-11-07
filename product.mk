f1877ceb3cf0ec9e3e6d55413e5db929595ff99f
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:56:45 -0800

    extra: Disable RescueParty
    RescueParty's aggressive approach to error recovery almost never helps
    with the problems we're likely to face, and its risk of data loss is far
    too high. Force-disable it entirely and remove all other conditions,
    especially those that depend on data integrity in the first place, to
    reduce the risk of RescueParty triggering falsely and causing data loss.
    
    Note that it is still possible to force-enable RescueParty for testing
    purposes using the persist.sys.enable_rescue property.
    
    Change-Id: I443a0750e288125943b3ca4872f9502ef8ab3b02
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- product.mk
+++ product.mk
@@ -2,3 +2,7 @@
 # Default ADB shell prompt
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.adb.shell=/system/xbin/bash
+
+# Disable RescueParty due to high risk of data loss
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
