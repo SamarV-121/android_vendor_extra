486dd5e461fadc316de1948a09cbfe70d58f0412
SamarV-121 <samarvispute121@gmail.com>
2021-02-07 21:30:07 +0530

    extra: Enable OTA support
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Change-Id: I8d005e02df2a8e5ccce199f4faeebb41d1e31898

--- product.mk
+++ product.mk
@@ -1,3 +1,6 @@
+## Overlays
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 ## Properties
 # Default ADB shell prompt
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -6,3 +9,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Disable RescueParty due to high risk of data loss
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
+
+## Updater
+PRODUCT_PROPERTY_OVERRIDES += \
+    lineage.updater.uri=https://raw.githubusercontent.com/SamarV-121/lineage_OTA/master/$(LINEAGE_BUILD).json
