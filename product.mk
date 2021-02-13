57ee6e5855d05e14ec1c54b36a63b0f7ecaf51e5
SamarV-121 <samarvispute121@gmail.com>
2021-02-13 21:15:55 +0530

    extra: Import prebuilt OnePlus Launcher
     * Modded by SKULSHADY
    
    Change-Id: I2375331d2241dd2d25853d6cf924342f41ab8862
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product.mk
+++ product.mk
@@ -40,13 +40,23 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
+# OnePlus launcher
+PRODUCT_PACKAGES += \
+    OnePlusRecentsProvider \
+    OPLauncher2
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/prebuilt/etc/permissions/privapp-permissions-op-launcher.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-op-launcher.xml \
+    vendor/extra/prebuilt/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
 	vendor/extra/overlay
 
 # Properties
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.build.fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+    ro.build.fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys \
+    ro.boot.vendor.overlay.static=false
 
 # Updater
 PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://raw.githubusercontent.com/SamarV-121/lineage_OTA/master/$(LINEAGE_BUILD).json
