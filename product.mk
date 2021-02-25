400c9d29bf7db273986ddf47e185f84e6e0d1340
SamarV-121 <samarvispute121@gmail.com>
2021-02-25 23:00:36 +0530

    extra: Remove OnePlus Launcher
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I84fd3df2a02799a9e0ade0b0a7b8acfca474738d

--- product.mk
+++ product.mk
@@ -39,23 +39,13 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
-# OnePlus launcher
-PRODUCT_PACKAGES += \
-    OnePlusRecentsProvider \
-    OPLauncher2
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/prebuilt/etc/permissions/privapp-permissions-op-launcher.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-op-launcher.xml \
-    vendor/extra/prebuilt/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml
-
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
 	vendor/extra/overlay
 
 # Properties
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.build.fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys \
-    ro.boot.vendor.overlay.static=false
+    ro.build.fingerprint=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
 
 # Updater
 PRODUCT_PROPERTY_OVERRIDES += \
