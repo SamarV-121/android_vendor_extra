51bf1e94cdbf2f201ab0f2e57340a75db4973e8f
SamarV-121 <samarvispute121@gmail.com>
2021-03-09 14:11:39 +0530

    extra: Update faceunlock impl
    Change-Id: I478222fdb72fa58ff06d0575be55d8a7e1212cb6
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product.mk
+++ product.mk
@@ -34,14 +34,16 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
 
-# Face Unlock
-ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+# Faceunlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
     FaceUnlockService
-TARGET_FACE_UNLOCK_SUPPORTED := true
-endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 
 # StichImage
 PRODUCT_PACKAGES += \
