215d6843b865bdfcea6862baa74fc3f0bb1f9c10
SamarV-121 <samarvispute121@gmail.com>
2021-04-11 21:58:02 +0530

    extra: Conditionally compile sepolicy for su
     * also ignore selinux neverallows
    
    Change-Id: Ib917ca13c52a8217bc5f526f7cb7f5d4f62e89d1
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,2 +1,5 @@
 # SEPolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/extra/sepolicy/private
+ifeq ($(WITH_SU), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/extra/sepolicy/su
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
