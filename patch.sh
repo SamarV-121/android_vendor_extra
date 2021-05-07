265d46ea495e39683c0a0a016faee9058006427b
SamarV-121 <samarvispute121@gmail.com>
2021-05-07 12:48:02 +0530

    extra: Improve patch script
    Change-Id: I01392066465a988b320f2cb8012f320601e1af56
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- patch.sh
+++ patch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+green="\033[01;32m"
+nocol="\033[0m"
+
 if [ -z "$*" ]; then
 	echo $'Option  Meaning\n-am     Patch using `git am`\n-ap     Patch using `git apply`\n-r      Revert patches'
+	exit 0
 fi
 
 if [ "$(basename "$PWD")" = extra ]; then
@@ -12,18 +16,19 @@ else
 
 fi
 
-find vendor/extra/patches -mindepth 1 -type d -printf '%P\n' | while IFS= read -r repository; do
+find vendor/extra/patches -name "0001*.patch" -printf '%P\n' | sed 's,/[^/]*$,,' | while IFS= read -r repository; do
 
 	LAST_COMMIT_HASH="$ROOT/${repository//\//_}.log"
 	PATCHES="$ROOT/vendor/extra/patches/$repository"
 
 	cd "$ROOT/$repository" || exit
+	echo -e "${green}Patching $repository ...${nocol}"
 
 	case $1 in
 	-am)
 		git rev-parse --short HEAD >"$LAST_COMMIT_HASH"
 		git am --abort 2>/dev/null
-		git am --keep-cr "$PATCHES"/*
+		git am --keep-cr -3 "$PATCHES"/*
 		;;
 	-ap)
 		git apply -v "$PATCHES"/*
@@ -33,10 +38,10 @@ find vendor/extra/patches -mindepth 1 -type d -printf '%P\n' | while IFS= read -
 		;;
 	-r)
 		if [ -e "$LAST_COMMIT_HASH" ]; then
-			git reset --hard "$(cat "$LAST_COMMIT_HASH")" 2>/dev/null
+			git reset --hard "$(cat "$LAST_COMMIT_HASH")"
 			rm "$LAST_COMMIT_HASH"
 		else
-			git reset --hard 2>/dev/null && git clean -df
+			git reset --hard && git clean -df
 		fi
 		;;
 	esac
