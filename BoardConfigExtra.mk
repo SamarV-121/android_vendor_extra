ec8931060fc0f5df5d318a78e681d65709b4978e
SamarV-121 <samarvispute121@gmail.com>
2021-06-26 11:26:44 +0530

    extra: Introduce App-lock
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I6b0f0f1aa224196407bfbf4107915744b20c6404

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,5 +1,7 @@
 # SEPolicy
-ifneq ($(filter true,$(WITH_SU) $(WITH_DEBUGGING)),)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/extra/sepolicy/private
+
+ifneq ($(filter true,$(WITH_SU) $(WITH_DEBUGGING)),)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += vendor/extra/sepolicy/eng
 SELINUX_IGNORE_NEVERALLOWS := true
 endif
