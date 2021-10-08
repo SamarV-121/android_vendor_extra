7e3a6e73076024843bb78694c7a027f06a492c22
SamarV-121 <samarvispute121@pm.me>
2021-10-08 11:28:08 +0530

    extra: Build Themepicker and messaging app
     * also kill QuickSearchBox
    
    Change-Id: I7e44e3c22fbfce4a6954581606072c679edc4ea2
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- product.mk
+++ product.mk
@@ -5,6 +5,12 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Packages
+PRODUCT_PACKAGES += \
+    RemovePackages \
+    ThemePicker \
+    messaging
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/permissions/privapp_whitelist_com.android.wallpaper.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_com.android.wallpaper.xml
