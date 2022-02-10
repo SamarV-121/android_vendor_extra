e50f5990c99a280e3d4b729d0ec2f396065f549c
jhenrique09 <jhsv09@gmail.com>
2022-02-10 11:25:28 -0300

    product: Enable support for Face Unlock
    Change-Id: I0beb2311a7fa469182b71c54f0773aac36b53354
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- product.mk
+++ product.mk
@@ -36,6 +36,17 @@ PRODUCT_PACKAGES += \
     MarkupGoogle \
     WallpaperPickerGoogleRelease
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 ## Fonts
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/fonts/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
