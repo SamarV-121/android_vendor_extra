c94f2e2fe4471447c03a3ee46c315c30a70829fd
SamarV-121 <samarvispute121@pm.me>
2022-09-12 18:39:46 +0530

    product: Sign build with private release keys
    Change-Id: I43d5ab60c1422876355ec619a05199d39be0ade3

--- product.mk
+++ product.mk
@@ -7,6 +7,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.recovery_update=true
 endif
 
+# Signing
+ifeq ($(SIGN_BUILD), true)
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
+endif
+
 # Updater
 PRODUCT_PRODUCT_PROPERTIES += \
     lineage.updater.uri=https://raw.githubusercontent.com/SamarV-121/lineage_OTA/master/$(LINEAGE_BUILD).json
